71b11b00bdead5ccddd17a28dfe23dcd89d20e4d
Betz <silverbez@gmail.com>
2020-02-09 16:35:11 +0200

    init

--- app/src/main/java/com/dev/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/dev/kotlinmessenger/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.dev.kotlinmessenger
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun registerClicked(view: View) {
+        Log.d("MainActivity", name_edittext_register.text.toString())
+        Log.d("MainActivity", email_edittext_register.text.toString())
+        Log.d("MainActivity", password_edittext_register.text.toString())
+    }
+
+    fun alreadyHaveAccountClicked(view: View) {}
 }
