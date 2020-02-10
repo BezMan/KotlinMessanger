90c879014ae95a119e14133e96f3d5c28171f389
Betz <silverbez@gmail.com>
2020-02-10 13:13:02 +0200

    fetchUsers

--- app/src/main/java/com/dev/kotlinmessenger/ComposeMessageActivity.kt
+++ app/src/main/java/com/dev/kotlinmessenger/ComposeMessageActivity.kt
@@ -1,13 +1,20 @@
 package com.dev.kotlinmessenger
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import com.xwray.groupie.kotlinandroidextensions.Item
 import kotlinx.android.synthetic.main.activity_compose_message.*
 
 class ComposeMessageActivity : AppCompatActivity() {
+
+    private val className: String = this.javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +31,30 @@ class ComposeMessageActivity : AppCompatActivity() {
         fetchUsers()
     }
 
+
     private fun fetchUsers() {
+        val ref = FirebaseDatabase.getInstance().getReference("/users")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+
+            override fun onDataChange(p0: DataSnapshot) {
+                p0.children.forEach {
+                    Log.d(className, it.toString())
+
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
 
     }
+
+
 }
+
+
 
 class UserItem(/*private val song: User*/) : Item() {
 
